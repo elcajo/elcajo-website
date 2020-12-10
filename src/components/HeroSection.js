67bd7eca8287752a590982cab7c37e8b3457288c
elcajo <46391075+elcajo@users.noreply.github.com>
2020-12-10 20:46:25 +0800

    test enchance

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -8,8 +8,8 @@ function HeroSection() {
 	return(
 		<div className='hero-container'>
 			<h1>This is IT</h1>
-			<p>A simple website created from React</p>
-			<p>Follow me on Github | github.com/elcajo</p>
+			<p align="center">A simple website created from React</p>
+			<p align="center">Follow me on Github | github.com/elcajo</p>
 			<div className='hero-btns'>
 				<Link to='/profile2'>
 					<Button className='btns' buttonStyle='btn--outline' buttonSize='btn--large' > ABOUT THE DEVELOPER </Button>
